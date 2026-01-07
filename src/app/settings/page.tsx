202def8628bf5c3a00f0a1d490dacd8fa8907fa5
Abdulganiyu Lukman <lukman4real001@gmail.com>
2026-01-07 05:54:25 +0000

    Now this should be padded to fit mobile screen view (_for element div_)

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -4,7 +4,7 @@ import { SettingsClient } from "@/components/settings/settings-client";
 export default function SettingsPage() {
     return (
         <MainLayout title="Settings">
-             <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
+             <div className="flex-1 space-y-4 p-2 md:p-8 pt-6">
                 <SettingsClient />
              </div>
         </MainLayout>
