083920b74759e9b19f3b42da4e5d979f66cd1384
Abdulganiyu Lukman <lukman4real001@gmail.com>
2026-01-07 04:45:27 +0000

    the navigation bar in this pop up is not functioning (_for element <Prim

--- src/components/transactions/transaction-form.tsx
+++ src/components/transactions/transaction-form.tsx
@@ -80,101 +80,102 @@ export function TransactionForm() {
 
   return (
     <Sheet open={isTransactionSheetOpen} onOpenChange={(open) => toggleTransactionSheet(open)}>
-      <SheetContent side="bottom" className="rounded-t-lg sm:max-w-2xl sm:mx-auto h-[75vh] flex flex-col">
-        <SheetHeader>
+      <SheetContent side="bottom" className="rounded-t-lg sm:max-w-2xl sm:mx-auto h-[75vh] flex flex-col p-0">
+        <SheetHeader className="p-6 pb-0">
           <SheetTitle>{editingTransaction ? 'Edit Transaction' : 'Create Transaction'}</SheetTitle>
           <SheetDescription>
             {editingTransaction ? 'Update the details of your transaction.' : 'Add a new sale or expense to your daily log.'}
           </SheetDescription>
         </SheetHeader>
         <ScrollArea className="flex-1">
+          <div className="px-6 py-4">
             <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 py-4 px-1">
-                <FormField
-                control={form.control}
-                name="type"
-                render={({ field }) => (
-                    <FormItem>
-                    <FormLabel>Type</FormLabel>
-                    <Select onValueChange={field.onChange} defaultValue={field.value}>
-                        <FormControl>
-                        <SelectTrigger>
-                            <SelectValue placeholder="Select transaction type" />
-                        </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                        <SelectItem value="sale">Sale</SelectItem>
-                        <SelectItem value="expense">Expense</SelectItem>
-                        </SelectContent>
-                    </Select>
-                    <FormMessage />
-                    </FormItem>
-                )}
-                />
-                <FormField
-                control={form.control}
-                name="amount"
-                render={({ field }) => (
-                    <FormItem>
-                    <FormLabel>Amount</FormLabel>
-                    <FormControl>
-                        <Input type="number" placeholder="0.00" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                    </FormItem>
-                )}
-                />
-                <FormField
-                control={form.control}
-                name="paymentMethod"
-                render={({ field }) => (
-                    <FormItem>
-                    <FormLabel>Payment Method</FormLabel>
-                    <Select onValueChange={field.onChange} defaultValue={field.value}>
-                        <FormControl>
-                        <SelectTrigger>
-                            <SelectValue placeholder="Select payment method" />
-                        </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                        <SelectItem value="cash">Cash</SelectItem>
-                        <SelectItem value="card">Card</SelectItem>
-                        <SelectItem value="transfer">Transfer</SelectItem>
-                        </SelectContent>
-                    </Select>
-                    <FormMessage />
-                    </FormItem>
-                )}
-                />
-                <FormField
-                control={form.control}
-                name="description"
-                render={({ field }) => (
-                    <FormItem>
-                    <FormLabel>Description</FormLabel>
-                    <FormControl>
-                        <Input placeholder="e.g. Sale of 2x T-shirts" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                    </FormItem>
-                )}
-                />
+              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                  <FormField
+                  control={form.control}
+                  name="type"
+                  render={({ field }) => (
+                      <FormItem>
+                      <FormLabel>Type</FormLabel>
+                      <Select onValueChange={field.onChange} defaultValue={field.value}>
+                          <FormControl>
+                          <SelectTrigger>
+                              <SelectValue placeholder="Select transaction type" />
+                          </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                          <SelectItem value="sale">Sale</SelectItem>
+                          <SelectItem value="expense">Expense</SelectItem>
+                          </SelectContent>
+                      </Select>
+                      <FormMessage />
+                      </FormItem>
+                  )}
+                  />
+                  <FormField
+                  control={form.control}
+                  name="amount"
+                  render={({ field }) => (
+                      <FormItem>
+                      <FormLabel>Amount</FormLabel>
+                      <FormControl>
+                          <Input type="number" placeholder="0.00" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                      </FormItem>
+                  )}
+                  />
+                  <FormField
+                  control={form.control}
+                  name="paymentMethod"
+                  render={({ field }) => (
+                      <FormItem>
+                      <FormLabel>Payment Method</FormLabel>
+                      <Select onValueChange={field.onChange} defaultValue={field.value}>
+                          <FormControl>
+                          <SelectTrigger>
+                              <SelectValue placeholder="Select payment method" />
+                          </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                          <SelectItem value="cash">Cash</SelectItem>
+                          <SelectItem value="card">Card</SelectItem>
+                          <SelectItem value="transfer">Transfer</SelectItem>
+                          </SelectContent>
+                      </Select>
+                      <FormMessage />
+                      </FormItem>
+                  )}
+                  />
+                  <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                      <FormItem>
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                          <Input placeholder="e.g. Sale of 2x T-shirts" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                      </FormItem>
+                  )}
+                  />
 
-                <DescriptionSuggester 
-                    currentDescription={currentDescription}
-                    pastTransactions={pastDescriptions}
-                    onSuggestionSelect={(suggestion) => form.setValue('description', suggestion)}
-                />
-                
-                <SheetFooter className="pt-4 bg-background sticky bottom-0">
-                <SheetClose asChild>
-                    <Button variant="outline">Cancel</Button>
-                </SheetClose>
-                <Button type="submit">Save Transaction</Button>
-                </SheetFooter>
-            </form>
+                  <DescriptionSuggester 
+                      currentDescription={currentDescription}
+                      pastTransactions={pastDescriptions}
+                      onSuggestionSelect={(suggestion) => form.setValue('description', suggestion)}
+                  />
+              </form>
             </Form>
+          </div>
         </ScrollArea>
+        <SheetFooter className="p-6 pt-4 border-t bg-background sticky bottom-0">
+          <SheetClose asChild>
+              <Button variant="outline" className="w-full sm:w-auto">Cancel</Button>
+          </SheetClose>
+          <Button type="submit" onClick={form.handleSubmit(onSubmit)} className="w-full sm:w-auto">Save Transaction</Button>
+        </SheetFooter>
       </SheetContent>
     </Sheet>
   );
